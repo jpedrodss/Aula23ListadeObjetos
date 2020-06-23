edb097686b56e0ae7a650d6528fdcc06a1b39490
João Pedro <jpedrodss@gmail.com>
2020-06-23 15:30:18 -0300

    projeto finalizado

--- Program.cs
+++ Program.cs
@@ -15,23 +15,31 @@ namespace Aula23ListadeObjetos
             produtos.Add(new Produto(4, "Intel Core i9-9900k Coffe Lake", 3.699f));
             produtos.Add(new Produto(5, "HyperX Cloud Mix Rose Gold", 1.349f));
 
-            Produto iphone = new Produto();
-            iphone.Codigo  = 1;
-            iphone.Nome    = "iPhone 12";
-            iphone.Preco   = 9.999f;
-
+            Console.ForegroundColor = ConsoleColor.Blue;
+            System.Console.WriteLine("Lista de produtos e preços:");
             foreach (Produto p in produtos){
                 System.Console.WriteLine($"{p.Nome} - R$ {p.Preco}.");
             }
-
-            produtos.RemoveAt(3);
 
             produtos.RemoveAll(x => x.Nome == "iPhone 11");
 
-            System.Console.WriteLine("\nLista alterada");
+            System.Console.WriteLine("\nLista alterada:\n");
             foreach (Produto p in produtos){
                 System.Console.WriteLine($"{p.Nome} - R$ {p.Preco}.");
             }
+            Console.ResetColor();
+
+            List<Cartao> cartoes = new List<Cartao>();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            cartoes.Add(new Cartao("Henrique", "567912363524", "Visa", "06/25", 459 ));
+            cartoes.Add(new Cartao("Maria", "164873519875", "MasterCard", "06/26", 564 ));
+            cartoes.Add(new Cartao("Jorge", "896749865238", "Elo", "06/24", 126 ));
+
+            foreach (Cartao c in cartoes){
+                System.Console.WriteLine($"\nTitular do Cartão: {c.Titular}.\nNumero: {c.Numero}.\nBandeira: {c.Bandeira}.\nVencimento: {c.Vencimento}.\nCódigo de Segurança (CVV): {c.CVV}.");
+            }
+            Console.ResetColor();
         }
     }
 }
